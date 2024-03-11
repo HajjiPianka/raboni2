4848d3688b69a671122c09cb85d4f167ae99f2cc
Pawelo112 <elopawelo112@gmail.com>
2024-03-11 20:40:12 +0100

    Comparator 0 errors fix

--- src/ComparatorTest.java
+++ src/ComparatorTest.java
@@ -1,6 +1,8 @@
 import java.time.Period;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
+import java.util.Comparator;
 
 public class ComparatorTest {
     public static void main(String[] args){
@@ -9,7 +11,7 @@ public class ComparatorTest {
 
         //Sort with Inner Class
         Collections.sort(personList, new Comparator<Person>(){
-            public int compare(Person p1, Person p2){return p1.getSurName().compareTo(compareTo(p2.getSurName()));}
+            public int compare(Person p1, Person p2){return p1.getSurName().compareTo(p2.getSurName());}
         });
 
         System.out.println("=== Sorted Asc SurName");
