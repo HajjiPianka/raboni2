34c0c985692888834394b1daac44924e14078151
Kubz <jak.przybylinski@gmail.com>
2024-03-11 13:25:18 +0100

    nametestold

---
+++ src/NameTestOld.java
@@ -0,0 +1,18 @@
+public class NameTestOld {
+    public public static void main(String[] args) {
+        System.out.println("\n=== NameTestOld ===");
+        List<Person> list1 = Person.createShortList();
+
+        // Print Names Western
+        System.out.println("\n===Print Western Names===");
+        for(Person person: list1) {
+            person.printWesternName();
+        }
+        
+        // Print Names Eastern
+        System.out.println("\n===Print Western Names===");
+        for(Person person: list1) {
+            person.printEasternName();
+        }
+    }
+}
