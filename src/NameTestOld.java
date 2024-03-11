8c00a6c0da3e583efec13d11bbd6579ea2a13fde
Pawelo112 <elopawelo112@gmail.com>
2024-03-11 13:38:11 +0100

    Maly fix

--- src/NameTestOld.java
+++ src/NameTestOld.java
@@ -1,5 +1,5 @@
 public class NameTestOld {
-    public public static void main(String[] args) {
+     public static void main(String[] args) {
         System.out.println("\n=== NameTestOld ===");
         List<Person> list1 = Person.createShortList();
 
